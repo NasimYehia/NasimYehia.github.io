e49164625b33396c2696317cef390f996533f988
Nasim Yehia <n.yehia@alphaomega-eng.com>
2024-03-28 20:20:40 +0200

    sec  commit

--- main.js
+++ main.js
@@ -14,8 +14,40 @@ const retryButton2 = document.getElementById("retryButton2");
 // Load the video
 videoPlayer2.src = "rocky.mp4";
 
-// Load subtitles (example format)
-const subtitles = [
+videoPlayer1.style.display = "block";
+videoPlayer2.style.display = "none";
+subtitleDisplay1.style.display = "block";
+subtitleDisplay2.style.display = "none";
+speechDisplay1.style.display = "block";
+speechDisplay2.style.display = "none";
+retryButton1.style.display = "block";
+retryButton2.style.display = "none";
+
+switchButton.addEventListener("click", () => {
+  chooseVideo = !chooseVideo; // Toggle between true and false
+  if (chooseVideo) {
+    videoPlayer1.style.display = "block";
+    videoPlayer2.style.display = "none";
+    subtitleDisplay1.style.display = "block";
+    subtitleDisplay2.style.display = "none";
+    speechDisplay1.style.display = "block";
+    speechDisplay2.style.display = "none";
+    retryButton1.style.display = "block";
+    retryButton2.style.display = "none";
+  } else {
+    videoPlayer1.style.display = "none";
+    videoPlayer2.style.display = "block";
+    subtitleDisplay1.style.display = "none";
+    subtitleDisplay2.style.display = "block";
+    speechDisplay1.style.display = "none";
+    speechDisplay2.style.display = "block";
+    retryButton1.style.display = "none";
+    retryButton2.style.display = "block";
+  }
+});
+
+// Load subtitles1 (example format)
+const subtitles1 = [
   { start: 0, end: 9, text: "in a small town in India" },
   { start: 9, end: 12, text: "lived a young man named Aladdin" },
   { start: 12, end: 14, text: "One day" },
@@ -118,23 +150,138 @@ const subtitles = [
   },
 ];
 
-let prevSub = subtitles[0];
-subtitleDisplay1.textContent = prevSub.text;
-// Display subtitles based on video time
+const subtitles2 = [
+  { start: 0, end: 4, text: "It is night time" },
+  { start: 4, end: 6, text: "That means It has bedtime" },
+  { start: 6, end: 8, text: "for Henry" },
+  { start: 8, end: 10, text: "Henry remember" },
+  { start: 10, end: 15, text: "to put away your toys" },
+  { start: 15, end: 17, text: "Mom weren't you telling me" },
+  { start: 17, end: 19, text: "for to hurry up and go to sleep" },
+  { start: 19, end: 23, text: "for As usual Henry does not" },
+  { start: 23, end: 25, text: "put away his scattered toys" },
+  { start: 25, end: 31, text: "while deep in sleep" },
+  { start: 31, end: 36, text: "what's that sound" },
+  { start: 36, end: 39, text: "while I think It has coming" },
+  { start: 39, end: 42, text: "from my room" },
+  { start: 42, end: 48, text: "while I am late because of you" },
+  { start: 48, end: 49, text: "go on" },
+  { start: 49, end: 51, text: "At least open the window" },
+  { start: 51, end: 54, text: "Henry completely startled" },
+  { start: 54, end: 56, text: "opens the window" },
+  { start: 56, end: 60, text: "And Rocky zooms out" },
+  { start: 60, end: 63, text: "uh oh It looks like" },
+  { start: 63, end: 65, text: "Hanrys pajama shirt" },
+  { start: 65, end: 69, text: "got caught on Rocky's wing" },
+  { start: 69, end: 72, text: "Mommy Help" },
+  { start: 72, end: 74, text: " Where are we going" },
+  { start: 74, end: 76, text: "Listen I'm late because of you" },
+  { start: 76, end: 78, text: "Anyway why are you" },
+  { start: 78, end: 80, text: "tagging along" },
+  { start: 80, end: 83, text: "I am sorry Rocky" },
+  { start: 83, end: 87, text: "From now on I promise" },
+  { start: 87, end: 89, text: "I will always clean up my room" },
+  { start: 89, end: 94, text: "Well we'll just have to see" },
+  { start: 94, end: 96, text: "Henry and Rocky" },
+  { start: 96, end: 98, text: "fly out into space" },
+  { start: 98, end: 102, text: "Wow An octopus star" },
+  { start: 102, end: 106, text: "a crown star car star" },
+  { start: 106, end: 113, text: "flower star This is so cool!" },
+  { start: 113, end: 114, text: "Wow" },
+  { start: 114, end: 120, text: "A twinkling treasure star too" },
+  { start: 120, end: 124, text: "Ooh Fresh popcorn star" },
+  { start: 124, end: 126, text: "and pizza cola star" },
+  { start: 135, end: 137, text: "Rolling round donut stars" },
+  { start: 138, end: 138, text: "look cool too" },
+  { start: 139, end: 140, text: "The holes in the middle" },
+  { start: 141, end: 142, text: "are like tunnels" },
+  { start: 143, end: 143, text: "As they fly" },
+  { start: 144, end: 145, text: "through the last donut star" },
+  { start: 148, end: 149, text: "Ah l have got chocolate" },
+  { start: 150, end: 151, text: "in my eye" },
+  { start: 152, end: 153, text: "I cannot see anything" },
+  { start: 156, end: 157, text: "here let me help you" },
+  { start: 162, end: 163, text: "whew thanks" },
+  { start: 164, end: 164, text: "anytime" },
+  { start: 167, end: 167, text: "Henry is happy" },
+  { start: 168, end: 168, text: "to have helped Rocky" },
+  { start: 171, end: 172, text: "Flying around and around" },
+  { start: 173, end: 174, text: "they finally land on" },
+  { start: 174, end: 176, text: "the most special star" },
+  { start: 176, end: 177, text: "where a bunch of" },
+  { start: 178, end: 178, text: "alien friends live" },
+  { start: 182, end: 182, text: "There Rocky and Henry" },
+  { start: 183, end: 184, text: "talk and dance" },
+  { start: 185, end: 187, text: "with their alien friends all night" },
+  { start: 199, end: 199, text: "at last" },
+  { start: 199, end: 201, text: "the time has come to leave" },
+  { start: 203, end: 204, text: "Now we have to go back" },
+  { start: 206, end: 206, text: "come again" },
+  { start: 207, end: 208, text: "You bet I will" },
+  { start: 222, end: 223, text: "as the two approach" },
+  { start: 224, end: 224, text: "Henrys house" },
+  { start: 226, end: 228, text: "Rocky next time you take off" },
+  { start: 229, end: 231, text: "make sure you take me too" },
+  { start: 232, end: 232, text: "well that depends on whether" },
+  { start: 234, end: 236, text: "you can keep your room clean" },
+  { start: 238, end: 239, text: "of course l can" },
+  { start: 240, end: 241, text: "I can put away toys" },
+  { start: 242, end: 243, text: "in fact call me" },
+  { start: 243, end: 244, text: "the king of tidiness" },
+  { start: 247, end: 247, text: "This was such" },
+  { start: 248, end: 248, text: "This was such" },
+  { start: 249, end: 249, text: "an awesome trip" },
+  { start: 250, end: 251, text: "For me too" },
+  { start: 253, end: 254, text: "thank you Rocky" },
+  { start: 255, end: 255, text: "as soon as" },
+  { start: 256, end: 257, text: "their heads hit the pillow" },
+  { start: 258, end: 260, text: "Henry and Rocky were snoring" },
+  { start: 262, end: 262, text: "in their dreams" },
+  { start: 263, end: 264, text: "they ventured back into space" },
+];
+let prevSub1 = subtitles1[0];
+let prevSub2 = subtitles2[0];
+subtitleDisplay1.textContent = prevSub1.text;
+subtitleDisplay2.textContent = prevSub2.text;
+// Display subtitles1 based on video time
+let chooseVideo = true;
+
 videoPlayer1.addEventListener("timeupdate", () => {
-  const currentTime = videoPlayer1.currentTime;
-  const currentSubtitle = subtitles.find(
-    (sub) => currentTime >= sub.start && currentTime <= sub.end
-  );
-  if (prevSub.start != currentSubtitle.start) {
-    if (accumulatedTranscript.toLowerCase() === prevSub.text.toLowerCase()) {
-      subtitleDisplay1.textContent = currentSubtitle
-        ? currentSubtitle.text
-        : "";
-      accumulatedTranscript = ""; // Reset the accumulated transcript after a successful match
-      prevSub = currentSubtitle;
-    } else {
-      videoPlayer1.pause();
+  if (chooseVideo === true) {
+    const currentTime = videoPlayer1.currentTime;
+    const currentSubtitle = subtitles1.find(
+      (sub) => currentTime >= sub.start && currentTime <= sub.end
+    );
+    if (prevSub1.start != currentSubtitle.start) {
+      if (accumulatedTranscript.toLowerCase() === prevSub1.text.toLowerCase()) {
+        subtitleDisplay1.textContent = currentSubtitle
+          ? currentSubtitle.text
+          : "";
+        accumulatedTranscript = ""; // Reset the accumulated transcript after a successful match
+        prevSub1 = currentSubtitle;
+      } else {
+        videoPlayer1.pause();
+      }
+    }
+  }
+});
+
+videoPlayer2.addEventListener("timeupdate", () => {
+  if (chooseVideo === false) {
+    const currentTime = videoPlayer2.currentTime;
+    const currentSubtitle = subtitles2.find(
+      (sub) => currentTime >= sub.start && currentTime <= sub.end
+    );
+    if (prevSub2.start != currentSubtitle.start) {
+      if (accumulatedTranscript.toLowerCase() === prevSub2.text.toLowerCase()) {
+        subtitleDisplay2.textContent = currentSubtitle
+          ? currentSubtitle.text
+          : "";
+        accumulatedTranscript = ""; // Reset the accumulated transcript after a successful match
+        prevSub2 = currentSubtitle;
+      } else {
+        videoPlayer2.pause();
+      }
     }
   }
 });
@@ -142,8 +289,19 @@ videoPlayer1.addEventListener("timeupdate", () => {
 let accumulatedTranscript = ""; // Accumulate recognized speech here
 
 //Display Speech
+
 videoPlayer1.addEventListener("timeupdate", () => {
-  speechDisplay1.textContent = accumulatedTranscript;
+  if (chooseVideo === true) {
+    speechDisplay1.textContent = accumulatedTranscript;
+  } else {
+    speechDisplay2.textContent = accumulatedTranscript;
+  }
+});
+
+videoPlayer2.addEventListener("timeupdate", () => {
+  if (chooseVideo === false) {
+    speechDisplay2.textContent = accumulatedTranscript;
+  }
 });
 
 const recognition = new webkitSpeechRecognition();
@@ -157,26 +315,46 @@ recognition.onresult = function (event) {
     accumulatedTranscript += " " + transcript; // Accumulate the recognized words
     accumulatedTranscript = accumulatedTranscript.trim();
     console.log("Accumulated Transcript:", accumulatedTranscript); // Debugging
-    speechDisplay1.textContent = accumulatedTranscript; // Update the display in real-time
-
-    // Check if the video is paused and if the transcript matches the current subtitle
-    const currentSubtitle = subtitles.find(
-      (sub) =>
-        videoPlayer1.currentTime >= sub.start &&
-        videoPlayer1.currentTime <= sub.end
-    );
-    if (
-      videoPlayer1.paused &&
-      prevSub &&
-      accumulatedTranscript.toLowerCase() === prevSub.text.toLowerCase()
-    ) {
-      videoPlayer1.play();
-      accumulatedTranscript = ""; // Reset the accumulated transcript after a successful match
-      subtitleDisplay1.textContent = currentSubtitle
-        ? currentSubtitle.text
-        : ""; // Update the subtitle display
-      prevSub = currentSubtitle;
+    if (chooseVideo === true) {
+      speechDisplay1.textContent = accumulatedTranscript; // Update the display in real-time
+      const currentSubtitle = subtitles1.find(
+        (sub) =>
+          videoPlayer1.currentTime >= sub.start &&
+          videoPlayer1.currentTime <= sub.end
+      );
+      if (
+        videoPlayer1.paused &&
+        prevSub1 &&
+        accumulatedTranscript.toLowerCase() === prevSub1.text.toLowerCase()
+      ) {
+        videoPlayer1.play();
+        accumulatedTranscript = ""; // Reset the accumulated transcript after a successful match
+        subtitleDisplay1.textContent = currentSubtitle
+          ? currentSubtitle.text
+          : ""; // Update the subtitle display
+        prevSub1 = currentSubtitle;
+      }
+    } else {
+      speechDisplay2.textContent = accumulatedTranscript;
+      const currentSubtitle = subtitles2.find(
+        (sub) =>
+          videoPlayer2.currentTime >= sub.start &&
+          videoPlayer2.currentTime <= sub.end
+      );
+      if (
+        videoPlayer2.paused &&
+        prevSub2 &&
+        accumulatedTranscript.toLowerCase() === prevSub2.text.toLowerCase()
+      ) {
+        videoPlayer2.play();
+        accumulatedTranscript = ""; // Reset the accumulated transcript after a successful match
+        subtitleDisplay2.textContent = currentSubtitle
+          ? currentSubtitle.text
+          : ""; // Update the subtitle display
+        prevSub2 = currentSubtitle;
+      }
     }
+    // Check if the video is paused and if the transcript matches the current subtitle
   }
 };
 
@@ -192,4 +370,14 @@ videoPlayer1.addEventListener("ended", () => {
 retryButton1.addEventListener("click", () => {
   accumulatedTranscript = ""; // Reset the accumulated transcript
   speechDisplay1.textContent = accumulatedTranscript; // Update the display in real-time
+});
+
+videoPlayer2.addEventListener("ended", () => {
+  accumulatedTranscript = "";
+});
+
+// Retry button event listener
+retryButton2.addEventListener("click", () => {
+  accumulatedTranscript = ""; // Reset the accumulated transcript
+  speechDisplay2.textContent = accumulatedTranscript; // Update the display in real-time
 });
